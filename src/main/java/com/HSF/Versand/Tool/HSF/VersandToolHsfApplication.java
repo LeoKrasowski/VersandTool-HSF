b5ae0aa59ca3a653d31c13d533dd45b0135f76ae
Leo <leokrasowski@gmail.com>
2025-12-15 09:55:58 +0100

    First commit

---
+++ src/main/java/com/HSF/Versand/Tool/HSF/VersandToolHsfApplication.java
@@ -0,0 +1,13 @@
+package com.HSF.Versand.Tool.HSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersandToolHsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersandToolHsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HSF/Versand/Tool/HSF/VersandToolHsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HSF.Versand.Tool.HSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersandToolHsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
