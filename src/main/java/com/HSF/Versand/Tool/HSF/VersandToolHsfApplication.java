89451e36f3628b46ee2cd463401ab9a333c9373a
Leo <leokrasowski@gmail.com>
2025-12-16 13:16:34 +0100

    minor changes app properties, maven

--- src/main/java/com/HSF/Versand/Tool/HSF/VersandToolHsfApplication.java
+++ src/main/java/com/HSF/Versand/Tool/HSF/VersandToolHsfApplication.java
@@ -8,6 +8,7 @@ public class VersandToolHsfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VersandToolHsfApplication.class, args);
+		System.out.println("HSF Versand Tool gestartet");
 	}
 
 }
