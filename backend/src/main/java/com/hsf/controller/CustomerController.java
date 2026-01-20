ff3f91c715f2a74e442c5a4a39091073efc3a2fd
Leo <leokrasowski@gmail.com>
2026-01-20 08:38:34 +0100

    GetMapping for Customer

--- backend/src/main/java/com/hsf/controller/CustomerController.java
+++ backend/src/main/java/com/hsf/controller/CustomerController.java
@@ -1,5 +1,9 @@
 package com.hsf.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hsf.dto.CustomerRequestDTO;
 import com.hsf.dto.CustomerResponseDTO;
+import com.hsf.dto.UserResponseDTO;
 import com.hsf.entity.Customer;
+import com.hsf.entity.User;
 import com.hsf.mapper.CustomerMapper;
 import com.hsf.service.CustomerService;
 
@@ -23,6 +29,20 @@ public class CustomerController {
         this.customerMapper = customerMapper;
     }
     // Define REST endpoints here (e.g., GET, POST, PUT, DELETE)
+
+    @GetMapping
+        public List<CustomerResponseDTO> getAll() {
+        
+        List<Customer> customers = customerService.getAllCustomers(); //List of customers from database
+        List<CustomerResponseDTO> customerResponseDTOs = new ArrayList<>(); //New lists of DTOs entities
+
+        for(Customer customer : customers){ //go via user list from database
+            CustomerResponseDTO customerResponseDTO = customerMapper.toDto(customer); //mapping from User to dto
+            customerResponseDTOs.add(customerResponseDTO); //save in list DTOs
+        }
+
+        return customerResponseDTOs;
+    }
 
     @PostMapping("/add")
         public CustomerResponseDTO addCustomer(@RequestBody CustomerRequestDTO customerRequestDTO){  //frontend layer
